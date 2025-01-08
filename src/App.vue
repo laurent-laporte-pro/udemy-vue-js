134ef086a2d5ea9776a83fc56ee1d1f8ceae6c0e
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2025-01-08 10:31:06 +0100

    use usage of `watch` (part 1)

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,28 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const title = ref<string>('Bienvenue sur Vue 3')
-const firstName = ref('Laurent')
-const lastName = ref('Laporte')
 
-const fullName = computed(() => `${firstName.value} ${lastName.value.toUpperCase()}`)
+const x = ref<number>(0)
+const debug = ref<string>('')
+const locked = ref<boolean>(false)
 
-const price = ref(123)
-const discountPercent = ref(10)
-const finalPrice = computed(() => price.value * (1 - discountPercent.value / 100))
-
-const fullName2 = computed({
-  get: () => {
-    if (lastName.value) {
-      return `${firstName.value} ${lastName.value.toUpperCase()}`
-    } else {
-      return firstName.value
-    }
-  },
-  set: (value: string) => {
-    if (!value.includes(' ')) {
-      firstName.value = value
-      lastName.value = ''
-    } else {
-      const parts = value.split(' ')
-      firstName.value = parts[0]
-      lastName.value = parts[1].charAt(0).toUpperCase() + parts[1].slice(1).toLowerCase()
-    }
-  }
+// Utilisation de watch pour observer une variable
+watch(x, (newX, oldX) => {
+  debug.value = `x = ${newX} (ancienne valeur : ${oldX})`
+  locked.value = newX > 5;
 })
+
 
 </script>
 
 <template>
   <main>
     <h1>{{ title }}</h1>
-    <p><strong>Nom :</strong> {{ lastName }}, <strong>Prénom :</strong> {{ firstName }}</p>
-    <p>{{ fullName }}</p>
-    <p>{{ fullName2 }}</p>
-    <p><label for="name">Modifier le nom :</label> <input name="name" v-model="fullName2" placeholder="Prénom Nom"></p>
-    <table>
-      <tr>
-        <th>Prix</th>
-        <th>Réduction</th>
-        <th>Prix final</th>
-      </tr>
-      <tr>
-        <td class="currency">{{ price }}</td>
-        <td class="percent">{{ discountPercent }}</td>
-        <td class="currency">{{ finalPrice }}</td>
-      </tr>
-    </table>
+    <p>{{ debug }}</p>
+    <input type="number" v-model="x" :disabled="locked" />
   </main>
 </template>
 
 <style scoped>
-.currency {
-  text-align: right;
-}
-
-.currency::after {
-  content: ' €';
-}
-
-.percent {
-  text-align: right;
-}
-
-.percent::after {
-  content: ' %';
-}
 </style>
