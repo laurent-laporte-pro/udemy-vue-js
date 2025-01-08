78d0fc13c1a658cf54b827c3d4c3dda3922ae72e
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2025-01-08 10:55:17 +0100

    use usage of `onMounted`

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,24 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const title = ref<string>('Bienvenue sur Vue 3')
+const todoTitle = ref<string>('')
 
-const question = ref<string>('')
-const answer = ref<string>('Une question possède un point d’interrogation (?)')
-const image = ref<string>('')
-const loading = ref<boolean>(false)
-
-type Result = {
-  answer: string
-  forced: boolean
-  image: string
-}
-
-watch(question, async (newQuestion: string) => {
-  if (newQuestion.includes('?')) {
-    answer.value = 'Je réfléchis…'
-    try {
-      loading.value = true
-      const res = await fetch('https://yesno.wtf/api')
-      const obj: Result = await res.json()
-      answer.value = obj.answer
-      image.value = obj.image
-    } catch (error) {
-      answer.value = 'Désolé, je n’ai pas pu répondre à votre question.'
-    } finally {
-      loading.value = false
-    }
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    todoTitle.value = (await response.json()).title
+  } catch (error) {
+    console.error(error)
   }
 })
+
 </script>
 
 <template>
   <main>
     <h1>{{ title }}</h1>
-    <p>
-      <label for="question">Votre question : </label>
-      <input name="question" v-model="question" placeholder="Posez une question">
-    </p>
-    <p>{{ answer }}</p>
-    <p v-if="image"><img :src="image" alt="" /></p>
+    <p>{{ todoTitle }}</p>
   </main>
 </template>
 
