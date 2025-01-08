ef4fa217f44d9d931edfa6c4d8ac0ddd1f01d107
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2025-01-08 12:02:49 +0100

    using `<slot/>` in components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,17 @@ import { ref } from 'vue'
 import SubComp from './SubComp.vue'
 
 const title = ref<string>('Hello World!')
-const firstName = ref<string>('Laurent')
-const childMsg = ref<string>('-- no message yet --')
-
-const handleSubComponentEvent = (msg: string) => {
-  childMsg.value = msg
-}
 </script>
 
 <template>
   <h1>{{ title }}</h1>
-  <h3>{{ childMsg }}</h3>
-  <SubComp @sub-component-event="handleSubComponentEvent" title="Bonjour"/>
-  <SubComp @sub-component-event="handleSubComponentEvent" :title="firstName" color="darkGreen"/>
+  <SubComp>
+    <p>Contenu de la balise slot</p>
+  </SubComp>
+  <SubComp />
+  <SubComp>
+    <u>Exemple</u>
+  </SubComp>
 </template>
 
 <style scoped>
--- src/components/SubComp.vue
+++ src/components/SubComp.vue
@@ -1,24 +1,6 @@
 <script setup lang="ts">
-
-const props = defineProps({
-  title: String,
-  color: {
-    type: String,
-    default: 'rebeccapurple',
-    required: false
-  }
-})
-
-const emit = defineEmits<(e: 'sub-component-event', msg: string) => void>()
-
-const handleClick = () => {
-  console.log('Button clicked: ', props.title)
-  emit('sub-component-event', `Button clicked: ${props.title} from sub component`)
-}
-
 </script>
 
 <template>
-  <h2 :style="{color: color}">{{ props.title }}</h2>
-  <button @click="handleClick">Click</button>
+  <h2><slot>Texte par défaut</slot></h2>
 </template>
