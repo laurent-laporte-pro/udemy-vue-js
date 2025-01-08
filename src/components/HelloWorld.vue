581d0183e5bcc0d52a1c73a808a4b813edcbe068
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2025-01-08 11:56:18 +0100

    use `defineEmits` to send events from subcomponent to parent component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,18 @@ import SubComp from './SubComp.vue'
 
 const title = ref<string>('Hello World!')
 const firstName = ref<string>('Laurent')
+const childMsg = ref<string>('-- no message yet --')
+
+const handleSubComponentEvent = (msg: string) => {
+  childMsg.value = msg
+}
 </script>
 
 <template>
   <h1>{{ title }}</h1>
-  <SubComp title="Bonjour"/>
-  <SubComp :title="firstName" color="darkGreen"/>
+  <h3>{{ childMsg }}</h3>
+  <SubComp @sub-component-event="handleSubComponentEvent" title="Bonjour"/>
+  <SubComp @sub-component-event="handleSubComponentEvent" :title="firstName" color="darkGreen"/>
 </template>
 
 <style scoped>
--- src/components/SubComp.vue
+++ src/components/SubComp.vue
@@ -8,8 +8,17 @@ const props = defineProps({
     required: false
   }
 })
+
+const emit = defineEmits<(e: 'sub-component-event', msg: string) => void>()
+
+const handleClick = () => {
+  console.log('Button clicked: ', props.title)
+  emit('sub-component-event', `Button clicked: ${props.title} from sub component`)
+}
+
 </script>
 
 <template>
   <h2 :style="{color: color}">{{ props.title }}</h2>
+  <button @click="handleClick">Click</button>
 </template>
